b5b5de5b7e21d78b42e6b6c21af898f820ce3837
Steven <uuuq@msn.com>
2022-02-12 17:09:18 +0800

    1

---
+++ 1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/q1mi/hello"
+)
+
+const Pi = 3.14
+
+func main() {
+	const World = "世界"
+	a := 1
+	fmt.Println(a)
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+	fmt.Print("Happy", Pi, "你好\n")
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+	hello.SayHi()
+	b := "年后打工"
+	fmt.Printf("b类型为：%T\n", b)
+}
